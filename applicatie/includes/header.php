1216085e041c3184dcbcde78d17733f823f17950
smth1808 <MB.Straub@gmail.com>
2025-06-26 17:03:30 +0200

    Update header.php

--- applicatie/includes/header.php
+++ applicatie/includes/header.php
@@ -11,12 +11,12 @@ if (isset($_SESSION['role']) && $_SESSION['role'] === 'Personnel'):
             <input type="text" placeholder="Zoeken...">
             <button type="submit">Zoek</button>
         </div>
-        <div class="header-buttons">
-            <a href="Bezorgschema.php">
+        <div class="header-button">
+            <a href="bezorgschema.php">
                 <img src="afbeeldingen/Orderlijst.png" alt="Orderlijst"> Orderlijst
             </a>
             <a href="profiel.php">
-                <img src="afbeeldingen/user.png" alt="Account"> Account
+                <img src="afbeeldingen/user.png" alt="Profiel"> Profiel
             </a>
         </div>
     </header>
@@ -25,12 +25,12 @@ if (isset($_SESSION['role']) && $_SESSION['role'] === 'Personnel'):
     <!-- Header voor klanten of niet-ingelogde bezoekers -->
     <header class="header">
         <img src="afbeeldingen/Pizza-logo.png" alt="Logo">
-        <div class="header-buttons">
-            <a href="winkelmand.php">
-                <img src="afbeeldingen/winkelmandje.png" alt="Winkelmand"> Bestelling
+        <div class="header-button">
+            <a href="bestelling.php">
+                <img src="afbeeldingen/winkelmandje.png" alt="Bestelling"> Bestelling
             </a>
             <a href="profiel.php">
-                <img src="afbeeldingen/user.png" alt="Account"> Account
+                <img src="afbeeldingen/user.png" alt="Profiel"> Profiel
             </a>
         </div>
     </header>
