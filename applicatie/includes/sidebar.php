b2540122a4384407a8eff817192ba2fe764942b8
smth1808 <MB.Straub@gmail.com>
2025-06-26 15:15:36 +0200

    Create sidebar.php

---
+++ applicatie/includes/sidebar.php
@@ -0,0 +1,23 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+
+<aside class="sidebar">
+    <label class="hamburger-menu">
+        <input type="checkbox">
+    </label>
+    <nav>
+        <?php if (isset($_SESSION['role']) && $_SESSION['role'] === 'Personnel'): ?>
+            <a href="hoofdpagina_werknemer.php">Home</a>
+            <a href="profiel_werknemer.php">Profiel</a>
+            <a href="bezorgschema.php">Bezorgschema</a>
+        <?php else: ?>
+            <a href="hoofdpagina_klanten.php">Home</a>
+            <a href="menu.php">Menu</a>
+            <a href="profiel.php">Profiel</a>
+            <a href="bestelling.php">Bestelling</a>
+        <?php endif; ?>
+    </nav>
+</aside>
