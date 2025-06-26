460a8af42450932046221314ac28079e5b7d947d
smth1808 <MB.Straub@gmail.com>
2025-06-26 16:13:21 +0200

    Update footer.php

--- applicatie/includes/footer.php
+++ applicatie/includes/footer.php
@@ -1,10 +1,4 @@
-<?php
-if (__FILE__ == realpath($_SERVER['SCRIPT_FILENAME'])) {
-    header("Location: menu.php"); // redirect naar hoofdpagina
-    exit;
-}
- 
-?>
+
 <footer>
 <a href="privacy_statement.php">Privacyverklaring</a>
 </footer>
