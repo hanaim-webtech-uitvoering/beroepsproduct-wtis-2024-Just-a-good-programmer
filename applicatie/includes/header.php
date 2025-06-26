719e059ca5486524232a471894d9f6d1bcb09dbb
smth1808 <MB.Straub@gmail.com>
2025-06-26 15:34:23 +0200

    Update header.php

--- applicatie/includes/header.php
+++ applicatie/includes/header.php
@@ -1,5 +1,7 @@
 <?php
-
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 if (isset($_SESSION['role']) && $_SESSION['role'] === 'Personnel'):
 ?>
     <!-- Header voor werknemers -->
