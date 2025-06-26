37bf389748e9d5f4f49bf078fe3e102a86d36ce2
smth1808 <MB.Straub@gmail.com>
2025-06-26 14:32:26 +0200

    Update header.php

--- applicatie/includes/header.php
+++ applicatie/includes/header.php
@@ -1,5 +1,20 @@
 <?php
+if (__FILE__ == realpath($_SERVER['SCRIPT_FILENAME'])) {
+    header("Location: menu.php"); // redirect to homepage
+    exit;
+}
 
- 
 ?>
 
+<header class="header">
+    <img src="afbeeldingen/Pizza-logo.png" alt="Logo">
+
+    <div class="header-buttons">
+        <a href="winkelmand.php">
+            <img src="afbeeldingen/winkelmandje.png" alt="Winkelmand"> Bestelling
+        </a>
+        <a href="profiel.php">
+            <img src="afbeeldingen/user.png" alt="Account"> Account
+        </a>
+    </div>
+</header>
